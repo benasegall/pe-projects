e3c12fbd91801c212423999167fefa50536e3b49
Ben Segall <benasegall@gmail.com>
2022-09-27 21:49:49 +0200

    Add Business Card Styles

---
+++ madLibs.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+		<?php
+			$noun = "Stacey";
+			$pluralNoun = "rolling";
+			$verb = "scream";
+			$plural = "eyes";
+			$adjective = "the most ever";
+
+			echo "<p>" . "Today, every student has a computer small enough to fit into their " . $noun . ". You can solve any math problem by simply pushing the computer’s little" . $plural . "</p>"
+		?>
+
+		<p>Computers can add, multiply, divide, and <?php echo $verb;?>. They can also <?php echo $verb;?> better than a human.</p>
+
+		<p>Some computers have their own <?=$plural?>. Other’s have <?=$adjective?> screen that shows all kinds of <?=$pluralNoun?> and <?=$adjective?> figures.</p>
+	</body>
+</html>
