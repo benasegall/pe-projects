ca87ce610f6057cdd24713f91f0e1818412a9951
Ben Segall <benasegall@gmail.com>
2022-10-01 01:10:54 +0200

    monsters inc

--- monsters/index.php
+++ monsters/index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Adopt a Monster</title>
+
+	<meta name="description" content="Adopt a Monster">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+	<h1>National Center for Monster Adoption</h1>
+	<p class="intro">Hello! Welcome to the National Center for Monster Adoption. Our monsters adorable little monsters could be yours. We prefer our adopters to come and meet them in person but we also do domestic shipping. If you are interested do not hesitate to click the green button for more information about!</p>
+
 <?php
 
 $shadow = [
@@ -6,7 +24,7 @@ $shadow = [
 	"favoriteFood" => "Building Materials",
 	"age" => 6,
 	"adopted" => "false",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "shadow.jpg",
 ];
 
 $orangina = [
@@ -15,7 +33,7 @@ $orangina = [
 	"favoriteFood" => "friedChicken",
 	"age" => 4,
 	"adopted" => "true",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "orangina.jpg",
 ];
 
 $mrBanana = [
@@ -24,7 +42,7 @@ $mrBanana = [
 	"favoriteFood" => "chicken",
 	"age" => 5,
 	"adopted" => "false",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "mr-banana.jpg",
 ];
 
 $missReadsALot = [
@@ -33,7 +51,7 @@ $missReadsALot = [
 	"favoriteFood" => "eggos",
 	"age" => 7,
 	"adopted" => "false",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "miss-reads-a-lot.jpg",
 ];
 
 $limabean = [
@@ -42,7 +60,7 @@ $limabean = [
 	"favoriteFood" => "feet",
 	"age" => 2,
 	"adopted" => "true",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "limabean.jpg",
 ];
 
 $fragoo = [
@@ -51,46 +69,58 @@ $fragoo = [
 	"favoriteFood" => "water",
 	"age" => 11,
 	"adopted" => "false",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "fragoo.jpg",
 ];
 
 $codey = [
-	"id" => "6",
+	"id" => "7",
 	"name" => "Codey",
 	"favoriteFood" => "water",
 	"age" => 11,
 	"adopted" => "false",
-	"portrait" => "https://peprojects.dev/images/portrait.jpg",
+	"portrait" => "codey.jpg",
 ];
 
 $monsters = [$shadow, $orangina, $mrBanana, $limabean, $fragoo, $codey];
 
-foreach ($monsters as $monster) {
-	$id = $monster["id"];
-	$name = $monster["name"];
-	$story = "The coolest monster here is " . $monster["name"] . " !";
-	$portrait = $monster["portrait"];
-	$status = $monster["adopted"];
+?>
 
-	if ($status == "true") {
-		$status = "adopted";
-	} else {
-		$status = "Up for adoption!";
-	}
+<ul class='monster-list'>
 
-	echo "<li class='monster'>";
+	<?php
 
-		echo
-			"<monster-card id=' " . $id . " '>" .
-			"<picuture class='portrait'>" .
-				"<img src='" . $portrait . "' width='100'>" .
-			"<h2 class='name'>" . $name . "</h2>" .
+	foreach ($monsters as $monster) {
+		$id = $monster["id"];
+		$name = $monster["name"];
+		$story = "The coolest monster here is " . $monster["name"] . " !";
+		$portrait = $monster["portrait"];
+		$status = $monster["adopted"];
+		$statusMessage = "Ready for adoption";
 
-			"<p class='story'>" . $story . "</p>" .
-			"<p class='status'>" . $status . "</p>" .
-			"</monster-card>";
+		if ($status == "true") {
+			$status = "adopted";
+			$statusMessage = "Found home";
+		} else {
+			$status = "not-adopted";
+		}
+		?>
 
-	echo"</li>";
+		<li class="monster <?=$status?>">
+			<monster-card id="<?=$id?>">
+				<picture class="portrait">
+					<img src="<?=$portrait?>" alt="Portrait of <?=$name?>" width='100'> 
+				</picture>
+				<h2 class="name"><?=$name?></h2>
+				<p class="story"><?=$story?></p>
+				<p class="status"><?=$statusMessage?></p>
+			</monster-card>
+		</li>
+
+		<?php
+
 }
 
+echo "</ul>";
 ?>
+</body>
+</html>
