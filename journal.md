3ed5bdd4013a89b81175648bed61b83153c44d88
Ben Segall <benasegall@gmail.com>
2022-10-12 00:46:34 +0200

    theme challenge + inline block

--- journal.md
+++ journal.md
@@ -3,10 +3,10 @@ by Benjamin Segall
 https://peprojects.dev/alpha-5/ben/
 
 # 10 11 22
-OK
+Deep into the file network of the layout theme challenge. Also scouring the web for more design inspiration I can adapt. the I think once I work at it enough I might use this as a selected work
 
 # 10 9 22
-Today I designed a style tile for a websites layout. Gathering inspiration, deciding on fonts and color schemes to apply as well as button designs and the like
+I designed a style tile for a websites layout. Gathering inspiration, deciding on fonts and color schemes to apply as well as button designs and the like
 
 # 10 4 22
 I have pretty much figured out how PHP is used with CSS as it is pretty similar to how HTML uses CSS
--- layout-theme-challenge/modules/call-to-action.php
+++ layout-theme-challenge/modules/call-to-action.php
@@ -5,7 +5,7 @@
 
 	<p class='calm-voice'>When high-speed rail is done right, it pulls cities closer together in ways that are difficult to ignore. The speed at which space-age trains can travel and the lack of traffic to contend with mean that commutes are often half of what it would take to drive the same distance. Better still, far less carbon is emitted, less space needs to be allocated to parking, and poorer residents, who are less likely to own cars, are able to travel. Strong public transportation options stimulate the economy by connecting employers with broader pools of workers and vice-versa and lift the value of properties located near stations.</p>
 
-	<a href='#'>
+	<a class="button" href='#'>
 		<span>Find out more</span>
 	</a>
 
