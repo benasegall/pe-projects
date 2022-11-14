fb44ed80c505f991a3482aaf39b30287d5545677
Ben Segall <benasegall@gmail.com>
2022-11-14 20:15:40 +0100

    need to fix grid

--- code/work.php
+++ code/work.php
@@ -5,36 +5,38 @@
 
 <title>Code - Benjamin Segall</title>
 
-<section class="work">
-	<div class="content">
-		<h2 class="name">Responsive design</h2>
-		<p class="lang">HTML, CSS</p>
-		<p class="descr">Description</p>
-		<ul class="project">
-			<li class="button"><a href="https://peprojects.dev/alpha-5/ben/projects/responsive-design/" target="_blank">Project</a></li>
-		</ul>
-	</div>
-</section>
+<div class="card-grid">
+	<section class="work">
+		<div class="card">
+			<h2 class="name">Responsive design</h2>
+			<p class="lang">HTML, CSS</p>
+			<p class="descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<ul class="project">
+				<li class="button"><a href="https://peprojects.dev/alpha-5/ben/projects/responsive-design/" target="_blank">Project</a></li>
+			</ul>
+		</div>
+	</section>
 
-<section class="work">
-	<div class="content">
-		<h2 class="name">Layout Concept</h2>
-		<p class="lang">HTML, CSS, PHP</p>
-		<p class="descr">Description</p>
-		<ul class="project">
-			<li class="button"><a href="https://peprojects.dev/alpha-5/ben/projects/theme-challenge/" target="_blank">Project</a></li>
-		</ul>
-	</div>
-</section>
+	<section class="work">
+		<div class="card">
+			<h2 class="name">Layout Concept</h2>
+			<p class="lang">HTML, CSS, PHP</p>
+			<p class="descr">Description</p>
+			<ul class="project">
+				<li class="button"><a href="https://peprojects.dev/alpha-5/ben/projects/theme-challenge/" target="_blank">Project</a></li>
+			</ul>
+		</div>
+	</section>
 
-<section class="work">
-	<div class="content">
-		<h2 class="name">Project Name</h2>
-		<p class="lang">Used Languages</p>
-		<p class="descr">Description</p>
-		<ul class="project">
-			<li class="button"><a href="#">Case study</a></li>
-			<li class="button"><a href="#">Project</a></li>
-		</ul>
-	</div>
-</section>
+	<section class="work">
+		<div class="card">
+			<h2 class="name">Project Name</h2>
+			<p class="lang">Used Languages</p>
+			<p class="descr">Description</p>
+			<ul class="project">
+				<li class="button"><a href="#">Case study</a></li>
+				<li class="button"><a href="#">Project</a></li>
+			</ul>
+		</div>
+	</section>
+</div>
