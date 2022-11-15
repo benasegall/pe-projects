787b93afee2c594b7afb592b329139a502e41c6e
Ben Segall <benasegall@gmail.com>
2022-11-15 04:04:46 +0100

    favicon

--- meta.php
+++ meta.php
@@ -8,6 +8,9 @@
 	<meta name="description" content="Portfolio of Full-Stack Developer Benjamin Segall based in New York.">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link rel="icon" href="images/favicon-green.svg">
+	<link rel="icon" href="../images/favicon-green.svg">
+
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="../css/reset.css">
 	
