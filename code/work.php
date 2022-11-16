b65ae1fb91c218e954a0b90b3f5ccc8dc21acde9
Ben Segall <benasegall@gmail.com>
2022-11-16 04:36:42 +0100

    off white

--- code/work.php
+++ code/work.php
@@ -10,7 +10,7 @@
 		<div class="card">
 			<h2 class="name">Responsive design</h2>
 			<p class="lang">HTML, CSS</p>
-			<p class="descr">Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Minus, recusandae.</p>
+			<p class="descr">Using CSS to automatically resize a webpage for different screen sizes and viewports.</p>
 			<ul class="project">
 				<li><a class="button" href="https://peprojects.dev/alpha-5/ben/projects/responsive-design/" target="_blank">Project</a></li>
 			</ul>
@@ -21,14 +21,14 @@
 		<div class="card">
 			<h2 class="name">Layout Concept</h2>
 			<p class="lang">HTML, CSS, PHP</p>
-			<p class="descr">Description</p>
+			<p class="descr">A first iteration using PHP for making a dynamic and interactive webpage.</p>
 			<ul class="project">
 				<li><a class="button" href="https://peprojects.dev/alpha-5/ben/projects/theme-challenge/" target="_blank">Project</a></li>
 			</ul>
 		</div>
 	</section>
 
-	<section class="work">
+	<!-- <section class="work">
 		<div class="card">
 			<h2 class="name">Project Name</h2>
 			<p class="lang">Used Languages</p>
@@ -38,5 +38,5 @@
 				<li><a class="button" href="#">Project</a></li>
 			</ul>
 		</div>
-	</section>
+	</section> -->
 </div>
