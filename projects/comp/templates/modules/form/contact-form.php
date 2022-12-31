9b3a7946d80a8f3c82a2f8d1e61f550d26ceedd6
Ben Segall <benasegall@gmail.com>
2022-12-31 21:59:08 +0100

    Contact page testing 6

--- projects/comp/templates/modules/form/contact-form.php
+++ projects/comp/templates/modules/form/contact-form.php
@@ -1,21 +1,28 @@
 <?php
 
 if (isset($_POST['submit'])) {
-$to = "benasegall@gmail.com"; // this is your Email address
-$from = $_POST['user_email']; // this is the sender's Email address
+$to = "benasegall@gmail.com";
+$from = $_POST['user_email'];
 $name = $_POST['user_name'];
-$subject = "Form submission";
-$subject2 = "Copy of your form submission";
+$subject = "Site contact form";
+$subjectCopy = "Copy of your form submission";
 $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['user_message'];
+$messageCopy = "Here is a copy of your message " . $name . "\n\n" . $_POST['user_message'];
 
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
+$header = "From:" . $from;
+$header2 = "From:" . $to;
 
-mail($to,$subject,$message,$headers);
-mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+// mail($to, $subject, $message, $header);
+// mail($from, $subjectCopy, $messageCopy, $header2); // sends a copy of the message to the sender
 
-echo "Message Sent. Thank you " . $name . ", I'll be in contact.";
+// echo "Your message sent. Thank you " . $name . ", I'll be in contact.";
+
+	if (mail($to, $subject, $message, $header) && mail($from, $subjectCopy, $messageCopy, $header2)) {
+		echo "Your message sent. Thank you " . $name . ", I'll be in contact.";
+	} else {
+		echo "Sorry, an error occcured. The message did not send.";
+	}
+
 }
 
 ?>
