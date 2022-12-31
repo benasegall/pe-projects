3b35b2e118b1baa3484ca4a7c7ef7dbaf625ef7d
Ben Segall <benasegall@gmail.com>
2022-12-31 21:17:06 +0100

    Contact page testing 1

--- projects/comp/templates/modules/form/template.php
+++ projects/comp/templates/modules/form/template.php
@@ -1,9 +1,17 @@
-<p>Fill out this quick form to get in touch, or just write me through email.</p>
+<p>Fill out this quick form to get in touch through email.</p>
 
 <form class="contact-form" method="post">
-	<input type="text" name="name" placeholder="Name">
-	<input type="text" name="email" placeholder="Email">
-	<input type="text" name="subject" placeholder="Subject">
-	<textarea name="message" placeholder="Message"></textarea>
+	<label for="name">Name</label> <br> <input type="text" id="name" name="user_name" />
+	<br>
+
+	<label for="email">Email</label> <br> <input type="text" id="email" name="user_email" />
+	<br>
+
+	<label for="phone">Phone</label> <br> <input type="text" id="phone" name="user_phone" />
+	<br>
+
+	<label for="message">Message</label> <br> <textarea type="text" id="message" name="user_messae" rows="5" cols="30"></textarea>
+	<br>
+
 	<button type="submit" name="submit">Send</button>
 </form>
