1c7acc700986814b0b40bd8dbb74352fabd9a6ae
Ben Segall <benasegall@gmail.com>
2023-01-04 02:07:59 +0100

    form

--- projects/comp/about.php
+++ projects/comp/about.php
@@ -2,7 +2,7 @@
 
 			<div class="content-wrap">
 
-				<header class="head opener head-spacing align-content-Cone-Rone">
+				<header class="head opener head-spacing">
 					<?php 
 					$title = "About";
 
@@ -13,34 +13,34 @@
 
 				</header>
 
-				<section class="info align-content-Cone-Rtwo">
+				<section class="info">
 					<?php
 					$info =
 					'<h2 class="subhead p-spacing">My Background</h2>
 					<p class="p-spacing">My high school and college years set the footing for my career path. In high school I enrolled myself in a graphic design program at a vocational training school. This was my first experience in a design field. Later on I took courses at the Fashion Institute of Technology in New York. In college I pursued a marketing degree but was not fully connected with this field of study.</p>';
 
-					include('templates/modules/page-info/template.php')
+					//include('templates/modules/page-info/template.php')
 					?>
 				</section>
 
-				<section class="info align-content-Ctwo-Rtwo">
+				<section class="info">
 					<?php
+					$heading = 'Present';
 					$info =
-					'<h2 class="subhead p-spacing">Present</h2>
-					<p class="p-spacing">Once the pandemic hit, I decided to take a leave of absence from school and moved to New York. There I discovered Codesmith and took their Computer Science prep course. This was my first significant experience with software development.</p>
-					<p class="p-spacing">After this, I decided instead of returning to campus I would pursue a different route and was accepted into the design oriented software development mentorship program at Perpetual Education.</p>';
+					'<p>Once the pandemic hit, I decided to take a leave of absence from school and moved to New York. There I discovered Codesmith and took their Computer Science prep course. This was my first significant experience with software development.</p>
+					<p>After this, I decided instead of returning to campus I would pursue a different route and was accepted into the design oriented software development mentorship program at Perpetual Education.</p>';
 
 					include('templates/modules/page-info/template.php')
 					?>
 				</section>
 
-				<section class="info align-content-Cone-Rthree">
+				<section class="info">
 					<?php
 					$info =
 					'<h2 class="subhead p-spacing">Goals</h2>
 					<p class="p-spacing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam non, in, labore voluptatibus velit quas repudiandae sint eius impedit voluptas.</p>';
 
-					include('templates/modules/page-info/template.php')
+					//include('templates/modules/page-info/template.php')
 					?>
 				</section>
 
@@ -50,7 +50,7 @@
 					'<h2 class="subhead p-spacing">Outside of Work</h2>
 					<p class="p-spacing">Beyond staring into my code editor I spend my days with my dog, at lunch, on FaceTime or waiting for a favorable wind. I\'m into architecture, interior design, video games, video essays and artisanal teas.</p>';
 
-					include('templates/modules/page-info/template.php')
+					//include('templates/modules/page-info/template.php')
 					?>
 				</section>
 
--- projects/comp/templates/modules/page-info/template.php
+++ projects/comp/templates/modules/page-info/template.php
@@ -1,1 +1,4 @@
-<?=$info?>
+<div class="page-info">
+	<h2 class="subhead p-spacing"><?=$heading?></h2>
+	<?=$info?>
+</div>
