fce6cb23e52ac7e20df228ab0410d571faa15a4f
Ben Segall <benasegall@gmail.com>
2023-01-16 04:47:19 +0100

    Site footer fix

--- templates/modules/site-footer/template.php
+++ templates/modules/site-footer/template.php
@@ -1,13 +1,9 @@
-<footer class="site-footer">
-
-	<nav>
-		<ul>
-			<li><a href="?page=style-guide">Style Guide</a></li>
-			<li><a href="?page=resume">Resume</a></li>
-		</ul>
-	</nav>
-
-</footer>
+<nav>
+	<ul>
+		<li><a href="?page=style-guide">Style Guide</a></li>
+		<li><a href="?page=resume">Resume</a></li>
+	</ul>
+</nav>
 
 <script>
 	document.getElementById("year").innerHTML = new Date().getFullYear();
