f686f4e43804420380c2ba0578a91534a06d5503
Ben Segall <benasegall@gmail.com>
2023-01-16 21:43:14 +0100

    Resume Completed : todo Print button to PDF

--- templates/modules/resume/template.php
+++ templates/modules/resume/template.php
@@ -1,40 +1,36 @@
-<section>
+<section class="resume">
 
-	<h2><a href="javascript:if(window.print)window.print()">Print</a></h2>
-
-	<h2>Summary Statement</h2>
-	<p>Dynamic front-end developer looking to bring his passion for responsive design to a communicative team setting, with the goal of delivering frictionless experiences across devices</p>
+	<h2>Career Objective</h2>
+	<p>Dynamic front-end developer looking to bring his skills and passion for responsive website design, adaptation and maintenance to a communicative team setting, with the goal of delivering frictionless experiences across devices.</p>
 
 	<h2>Technical Skills</h2>
 	<ul>
-		<li>HTML</li>
-		<li>CSS</li>
-		<li>PHP</li>
-		<li>JavaScript</li>
-		<li>jQuery</li>
-		<li>Python</li>
-		<li>Wordpress</li>
-		<li>Affinity Designer</li>
+		<li><h3>Programming Languages</h3>: HTML, CSS, PHP, JavaScript, jQuery, Python</li>
+		<li><h3>Tools & Frameworks</h3>: Wordpress, Affinity Designer, GitHub, Agile/Scrum</li>
 	</ul>
 
 	<h2>Education</h2>
-	<p>Mentorship Program at Perpetual Education</p>
-	<p>July 2022 - Present</p>
+	<div class="date-handle">
+		<h3>Mentorship Program, Perpetual Education</h3>
+		<p>July 2022 – Present</p>
+	</div>
 	<ul>
 		<li>Enrolled in an in-depth mentorship program that taught the process of software design and development</li>
 		<li>Shaped professional skills while familiarizing with programming languages, application frameworks, and drawing tools</li>
 	</ul>
-
-	<p>Computer Science Prep at Codesmith</p>
-	<p>September 2021 - January 2022</p>
+	<div class="date-handle">
+		<h3>Computer Science Prep, Codesmith</h3>
+		<p>September 2021 - January 2022</p>
+	</div>
 	<ul>
 		<li>Learned fundamental JavaScript concepts, the foundations of programming languages, engineering best practices, and technical communication skills</li>
 	</ul>
-
-	<p>Brookdale Community College</p>
-	<p>September 2018 - May 2021</p>
+	<div class="date-handle">
+		<h3>Brookdale Community College</h3>
+		<p>September 2018 - May 2021</p>
+	</div>
 	<ul>
 		<li>Non-Degree</li>
 	</ul>
-
+	<h2><a href="javascript:if(window.print)window.print()">Print</a></h2>
 </section>
