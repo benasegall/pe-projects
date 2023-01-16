1b487154205f1fc006dbdab6d5ecf65216dbaa40
Ben Segall <benasegall@gmail.com>
2023-01-16 16:22:51 +0100

    Resume html

--- templates/modules/resume/template.php
+++ templates/modules/resume/template.php
@@ -1,45 +1,40 @@
 <section>
-<h2>Benjamin Segall</h2>
-<a href="javascript:if(window.print)window.print()">Print</a>
-<p>New York, NY — (732) 947-8414 — benasegall@gmail.com</p>
-<h3>Profile</h3>
-<p>Dynamic front-end developer looking to bring his passion for responsive design to a communicative team setting, with the goal of delivering frictionless experiences across devices</p>
-<h3>Education</h3>
-<p>Mentorship Program at Perpetual Education</p>
-<p>July 2022 - Present</p>
-<ul>
-<li>Enrolled in an in-depth mentorship program that taught the process of software design and development</li>
-<li>Shaped professional skills while familiarizing with programming languages, application frameworks, and drawing tools</li>
-</ul>
-<p>Computer Science Prep at Codesmith</p>
-<p>September 2021 - January 2022</p>
-<ul>
-<li>Learned fundamental JavaScript concepts, the foundations of programming languages, engineering best practices, and technical communication skills</li>
-</ul>
-<p>Brookdale Community College</p>
-<p>September 2018 - May 2021</p>
-<p>Relevant Coursework</p>
-<ul>
-<li>Introduction to Business</li>
-<li>Introduction to Psychology II</li>
-<li>Public Speaking</li>
-<li>Money Management and Personal Finance</li>
-<li>Introduction to Marketing</li>
-<li>English Composition: Writing Process</li>
-</ul>
-<h3>Technical Skills</h3>
-<ul>
-<li>HTML</li>
-<li>CSS</li>
-<li>PHP</li>
-<li>JavaScript</li>
-<li>Python</li>
-</ul>
-<ul>
-<li>Wordpress</li>
-<li>Affinity Designer</li>
-</ul>
-<h3>Languages</h3>
-<p>English - Native</p>
-<p>French - Limited Working Proficiency</p>
+
+	<h2><a href="javascript:if(window.print)window.print()">Print</a></h2>
+
+	<h2>Summary Statement</h2>
+	<p>Dynamic front-end developer looking to bring his passion for responsive design to a communicative team setting, with the goal of delivering frictionless experiences across devices</p>
+
+	<h2>Technical Skills</h2>
+	<ul>
+		<li>HTML</li>
+		<li>CSS</li>
+		<li>PHP</li>
+		<li>JavaScript</li>
+		<li>jQuery</li>
+		<li>Python</li>
+		<li>Wordpress</li>
+		<li>Affinity Designer</li>
+	</ul>
+
+	<h2>Education</h2>
+	<p>Mentorship Program at Perpetual Education</p>
+	<p>July 2022 - Present</p>
+	<ul>
+		<li>Enrolled in an in-depth mentorship program that taught the process of software design and development</li>
+		<li>Shaped professional skills while familiarizing with programming languages, application frameworks, and drawing tools</li>
+	</ul>
+
+	<p>Computer Science Prep at Codesmith</p>
+	<p>September 2021 - January 2022</p>
+	<ul>
+		<li>Learned fundamental JavaScript concepts, the foundations of programming languages, engineering best practices, and technical communication skills</li>
+	</ul>
+
+	<p>Brookdale Community College</p>
+	<p>September 2018 - May 2021</p>
+	<ul>
+		<li>Non-Degree</li>
+	</ul>
+
 </section>
