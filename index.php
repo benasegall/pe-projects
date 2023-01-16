4be888c99a18969d74c10cde5f995a59e8f0f31d
Ben Segall <benasegall@gmail.com>
2023-01-16 05:40:48 +0100

    W3C corrections and metadata

--- index.php
+++ index.php
@@ -9,6 +9,24 @@
 
 	<title>Benjamin Segall</title>
 
+  <!-- HTML Meta Tags -->
+  <meta name="description" content="Portfolio of American Front-end developer Benjamin Segall based on the East Coast.">
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://hellobenjamin.dev">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Benjamin Segall">
+  <meta property="og:description" content="Portfolio of American Front-end developer Benjamin Segall based on the East Coast.">
+  <meta property="og:image" content="images/favicon.svg">
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="hellobenjamin.dev">
+  <meta property="twitter:url" content="https://hellobenjamin.dev">
+  <meta name="twitter:title" content="Benjamin Segall">
+  <meta name="twitter:description" content="Portfolio of American Front-end developer Benjamin Segall based on the East Coast.">
+  <meta name="twitter:image" content="images/favicon.svg">
+
 	<link rel="icon" href="images/favicon.svg">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
--- templates/components/contact-form/template.php
+++ templates/components/contact-form/template.php
@@ -9,16 +9,16 @@
 
 <form class="contact-form" method="post">
 	<div class="field">
-		<label for="name">Name</label> <input type="text" id="name" name="user_name" required="required" />
+		<label for="name">Name</label> <input type="text" id="name" name="user_name" required="required"/>
 	</div>
 
 	<div class="field">
-		<label for="email">Email</label> <input type="text" id="email" name="user_email" required="required" />
+		<label for="email">Email</label> <input type="text" id="email" name="user_email" required="required"/>
 	</div>
 
-	<label for="subject">Subject</label> <input type="text" id="subject" name="email_subject" required="required" />
+	<label for="subject">Subject</label> <input type="text" id="subject" name="email_subject" required="required"/>
 
-	<label for="message">Message</label> <textarea type="text" id="message" name="user_message" required="required"></textarea>
+	<label for="message">Message</label> <textarea id="message" name="user_message" required="required"></textarea>
 
 	<button type="submit" name="submit">Send</button>
 </form>
