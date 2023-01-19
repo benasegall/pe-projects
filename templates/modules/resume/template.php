9f44dbc392ea92a1a63e955ced589f13f34f8269
Ben Segall <benasegall@gmail.com>
2023-01-19 04:34:38 +0100

    Print SVG

--- templates/modules/resume/template.php
+++ templates/modules/resume/template.php
@@ -70,5 +70,7 @@
 		</ul>
 	</div>
 
-	<h2><a class="print" href="javascript:if(window.print)window.print()">Print</a></h2>
+	<span class="print-wrap">
+		<a class="print" href="javascript:if(window.print)window.print()"><svg viewBox="0 0 24 24"><path fill="currentColor" d="M18 3H6v4h12m1 5a1 1 0 01-1-1 1 1 0 011-1 1 1 0 011 1 1 1 0 01-1 1m-3 7H8v-5h8m3-6H5a3 3 0 00-3 3v6h4v4h12v-4h4v-6a3 3 0 00-3-3z"></path></svg>Print</a>
+	</span>
 </section>
