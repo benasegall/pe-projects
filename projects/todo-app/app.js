65056d28d8706db9f27d033974a3442526630c97
Ben Segall <benasegall@gmail.com>
2023-01-27 01:39:44 +0100

    todo List to Constructor function

--- projects/todo-app/app.js
+++ projects/todo-app/app.js
@@ -1,61 +1,106 @@
-function Constructor(name, age) {
-	this.name = name,
-	this.age = age,
+// function Constructor(name, age) {
+// 	this.name = name,
+// 	this.age = age,
 
-	this.sound = function() {
-		alert(`${name}`);
-	}
-}
+// 	this.sound = function() {
+// 		alert(`${name}`);
+// 	}
+// }
 
-const toby = new Constructor("Tobuscus", 15);
-const rick = new Constructor("Richard", 2);
+// const toby = new Constructor("Tobuscus", 15);
+// const rick = new Constructor("Richard", 2);
 
-toby.sound();
-rick.sound();
+// toby.sound();
+// rick.sound();
 
-Constructor.prototype.greeting = function() {
-	console.log(`... ${this.name} says, "Hewo my hooman"`)
-}
+// Constructor.prototype.greeting = function() {
+// 	console.log(`... ${this.name} says, "Hewo my hooman"`)
+// }
 
-toby.greeting();
+// toby.greeting();
 
 
 
-const todoApp = {
+function todoApp() {
+	this.todos = [];
+	this.todosId = 0,
 
-	todos: [],
-	todoId: 0,
-
-	log(note = "") {
-		console.log(`–${note}`);
-		console.log("todo:", this.todos);
-		// debugger;
+	this.log = function(note = '') {
+	console.log(`-${note}`);
+	console.log('todo:', this.todos);
 	},
 
-	add(content) {
+	this.add = function(content) {
 		let todo = {
-			id: `${this.todoId++}`,
+			id: `${this.todosId++}`,
 			content: content
-		};
+		}
 		this.todos = [...this.todos, todo];
 		this.log(`Added ${content}`);
 	},
 
-	remove(id) {
+	this.remove = function(id) {
 		this.log(`Removed ${id}`);
+		this.todo.splice(id, 1);
+	},
+
+	this.update = function(id, newContent) {
 		this.todos.splice(id, 1);
-	},
+		this.add(newContent);
+	}
 
-	completed(id) {
-		this.todos[id].completed = true;
-		this.log(`completed ${this.todos[id].content}`);
-	},
-
-	update(newContent) {
-		this.todos[id].content = newContent;
-	},
 }
 
-todoApp.add("Complete todo list day 2");
-todoApp.add("Complete Constructor functions and debugger");
-todoApp.completed(1);
+const todo = new todoApp();
+
+todo.add("Brush your teeth");
+todo.add("Put on the dishwasher");
+todo.add("Complete a Constructor function");
+todo.update(3, "Onto the next lesson!");
+
+todoApp.prototype.completed = function(id) {
+	this.todos[id].completed = true;
+	this.log(`completed ${this.todos[id].content}`);
+}
+
+todo.completed(2);
+
+
+// const todoApp = {
+
+// 	todos: [],
+// 	todoId: 0,
+
+// 	log(note = "") {
+// 		console.log(`–${note}`);
+// 		console.log("todo:", this.todos);
+// 		// debugger;
+// 	},
+
+// 	add(content) {
+// 		let todo = {
+// 			id: `${this.todoId++}`,
+// 			content: content
+// 		};
+// 		this.todos = [...this.todos, todo];
+// 		this.log(`Added ${content}`);
+// 	},
+
+// 	remove(id) {
+// 		this.log(`Removed ${id}`);
+// 		this.todos.splice(id, 1);
+// 	},
+
+// 	completed(id) {
+// 		this.todos[id].completed = true;
+// 		this.log(`completed ${this.todos[id].content}`);
+// 	},
+
+// 	update(newContent) {
+// 		this.todos[id].content = newContent;
+// 	},
+// }
+
+// todoApp.add("Complete todo list day 2");
+// todoApp.add("Complete Constructor functions and debugger");
+// todoApp.completed(1);
