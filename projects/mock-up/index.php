a17ae339a4c9bd1d5c177891192375e81fd50277
Ben Segall <benasegall@gmail.com>
2023-01-31 02:01:57 +0100

    Jan 30 Mock-up

---
+++ projects/mock-up/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>mock-up</title>
+
+	<!-- <link rel="stylesheet" href="styles/site.css"> -->
+</head>
+<body>
+	<header>
+		<div class="structure">
+			<?php include('templates/components/nav/template.php'); ?>
+		</div>
+	</header>
+
+	<main>
+		<div class="stucture">
+			<section class="diptych">
+				<?php include('templates/modules/diptych/template.php'); ?>
+			</section>
+
+			<section></section>
+		</div>
+	</main>
+</body>
+</html>
---
+++ projects/mock-up/templates/components/nav/template.php
@@ -0,0 +1,18 @@
+<nav class="site-nav">
+	<ul>
+		<li class="logo"></li>
+	</ul>
+
+	<ul>
+		<li>Home</li>
+		<li>Updates</li>
+		<li>Services</li>
+		<li>Features</li>
+		<li>About Us</li>
+	</ul>
+
+	<ul>
+		<li></li>
+		<li><button>Login</button></li>
+	</ul>
+</nav>
