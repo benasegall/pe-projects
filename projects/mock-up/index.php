f8d54cd5f8a65a3be35aa83b312e81b4b4ec31ad
Ben Segall <benasegall@gmail.com>
2023-02-01 06:09:51 +0100

    mock-up section video

--- projects/mock-up/index.php
+++ projects/mock-up/index.php
@@ -5,6 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>mock-up</title>
 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+
 	<link rel="stylesheet" href="styles/site.css">
 </head>
 <body>
@@ -17,7 +21,7 @@
 	<main>
 		<?php include('templates/modules/diptych/template.php'); ?>
 
-		<section></section>
+		<?php include('templates/modules/video/template.php'); ?>
 	</main>
 </body>
 </html>
