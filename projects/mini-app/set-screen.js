f72636c9b40650e46feb2fa567c4a15de533012e
Ben Segall <benasegall@gmail.com>
2023-02-17 17:34:31 -0500

    min app home screen setup

--- projects/mini-app/set-screen.js
+++ projects/mini-app/set-screen.js
@@ -1,3 +1,7 @@
+import {
+	unitList
+} from './units.js';
+
 let $output = document.querySelector('[data-output="main"]');
 
 function setScreen(screen) {
@@ -7,19 +11,67 @@ function setScreen(screen) {
 	$output.innerHTML = pages[screen];
 }
 
+// for Add a filter search tool
+function filterItem(unitList, userFilter) {
+	unitList.forEach(function(description) {
+		if (description.includes(userFilter) {
+			return unit
+		})
+	})
+}
+
+function renderUnit(unitList) {
+	let unit = "";
+
+	unitList.forEach(function(unitItem) {
+		unit += 
+		`<div class="unit-card">
+			<img src="${unitItem.image}">
+			<h2>${unitItem.name}</h2>
+			<p>${unitItem.description}</p>
+			<button>Save for later</button>
+		</div>`;
+	})
+	return unit;
+}
+
+
 let pages = {};
 
 pages.logo = `
 	<h1>Landing</h1>
+	<section class="hero">
+		<h1>The easiest way to rent, buy & sell your home</h1>
+
+		<div class="search">
+			<form>
+				<input type="text" placeholder="Add a filter" name="filter">
+				<button>Go</button>
+			</form>
+		</div>
+	</section>
+
+	<section class='units'>
+		${renderUnit(unitList)}
+	</section>
+
 `;
+
+
 
 pages.savedHomes = `
 	<h1>Saved</h1>
+
 `;
+
+
 
 pages.addNew = `
 	<h1>New</h1>
+
 `;
+
+
 
 setScreen("logo"); // default landing page
 
---
+++ projects/mini-app/units.js
@@ -0,0 +1,24 @@
+let unitList = [
+	{
+		name: "name",
+		description: "description",
+		features: "dishwasher",
+		image: "./images/generic.jpeg"
+	},
+	{
+		name: "name",
+		description: "description",
+		features: "washer/dryer",
+		image: "./images/generic.jpeg"
+	},
+	{
+		name: "name",
+		description: "description",
+		features: "pets",
+		image: "./images/generic.jpeg"
+	}
+]
+
+export {
+	unitList
+};
